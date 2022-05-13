338fb9e539c41b3d4b8b707fed124482824ba475
Omar El Farouk GASSEM <oefgassem@gmail.com>
2022-05-13 20:00:49 +0100

    postgres

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:3.11.0b1-alpine3.15
+FROM python:3
 ENV PYTHONUNBUFFRED=1
 COPY ./requirments.txt /requirments.txt
 RUN pip install -r /requirments.txt
-RUN mkdir /app
-WORKDIR /app
-COPY ./app /app/
+RUN mkdir /src
+WORKDIR /src
+COPY ./src /src/
+
--- src/web/web/urls.py
+++ src/web/web/urls.py
@@ -1,4 +1,4 @@
-"""app URL Configuration
+"""web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", homePageView, name="home"),
+
 ]
